63abc99cfd83620741e8e1379a132cf53bb3a6cb
Marius Daniel Vasilcanu <74616980+MariusVasilcanu28@users.noreply.github.com>
2025-02-06 21:00:42 +0200

    Update useMovies.js

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -17,7 +17,7 @@ export function useMovies(query) {
         setError("");
 
         const res = await fetch(
-          `https://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&s=${query}`,
           { signal: controller.signal }
         );
 
