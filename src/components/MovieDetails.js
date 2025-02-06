c333c0751b1096740a2fc43a0ffaf396e2cdc6cd
Marius Daniel Vasilcanu <74616980+MariusVasilcanu28@users.noreply.github.com>
2025-02-06 21:15:29 +0200

    wip

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -63,7 +63,7 @@ export default function MovieDetails({
       setIsLoading(true);
 
       const res = await fetch(
-        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&i=${selectedId}`
       );
 
       const data = await res.json();
